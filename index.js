5ff6f14e36ca7bcf5d0a558e3956e8d0e993b439
CHIC043 <karsen.kapayou@students.msswarriors.org>
2026-04-17 15:19:42 +0000

    check

---
+++ index.js
@@ -0,0 +1,3 @@
+const introduction = "This is a line with a new line\n\tand a tab, plus a single quote (') and a double quote (\") and a backslash (\\).";
+
+document.getElementById("result").innerHTML = introduction;
